ce9b5d0f282bfeafcca2607e9657817561bd9e83
Matthew Moore <matt@crowdmob.com>
2014-04-05 09:50:29 -0700

    remove attr_accessible

--- app/models/active_mailer/email_user_association.rb
+++ app/models/active_mailer/email_user_association.rb
@@ -1,6 +1,7 @@
 module ActiveMailer
   class EmailUserAssociation < ActiveRecord::Base
-    attr_accessible :email_user_id
+    # attr_accessible :email_user_id
+    # Deprecated from Rails 4
     
     belongs_to :email_user
     belongs_to :emailable, :polymorphic => true
